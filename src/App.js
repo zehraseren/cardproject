5cefb25a1922d416c239621f65eeb6b3e35d5ecc
Zehra Şeren <fatmazehraseren@gmail.com>
2024-01-22 14:58:10 +0300

    AppBar kullanılarak header oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { AppBar, Toolbar } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <AppBar>
+        <Toolbar sx={{fontSize:"27px"}}>Kart Projesi</Toolbar>
+      </AppBar>
     </div>
   );
 }
